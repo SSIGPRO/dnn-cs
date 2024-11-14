d24b4d60e2fa23c547c93664a1784f4a16d5aa31
Alex Marchioni <alex.marchioni@unibo.it>
2024-11-14 15:09:38 +0100

    computation of supports and RSNR without sensing matrix orthogonalization

--- experiments/script.sh
+++ experiments/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-n=128
-N_corr=100000
-fs=256
-heart_rate=(60 100)
-ecg_corr_seed=42
+# n=128
+# N_corr=100000
+# fs=256
+# heart_rate=(60 100)
+# ecg_corr_seed=42
 
 # python ./experiments/generate_ecg.py \
 #     --size $N_corr \
@@ -75,6 +75,7 @@ ecg_corr_seed=42
 
 ###############################################################################
 
+
 corr=96af96a7ddfcb2f6059092c250e18f2a
 loc=0.25
 
@@ -87,6 +88,36 @@ eta_list=(
     0.9993  0.9995  0.9997  0.9998  0.99985  0.9999 
     0.99993 0.99995 0.99997 0.99998 0.999985 0.99999
 )
+
+algorithm="TSOC"
+for isnr in 35 25 45
+do
+    for encoder in "standard" "rakeness"
+    do
+        # for algorithm in "GR" "TSOC"
+        # do
+            for m in "${m_list[@]}"
+            do
+                for seed in "${seed_list[@]}"
+                do
+                    echo $isnr $encoder $algorithm $m $seed
+                    python experiments/compute_supports.py \
+                        --isnr $isnr \
+                        --algorithm $algorithm \
+                        --encoder $encoder \
+                        --measurements $m \
+                        --correlation $corr \
+                        --localization $loc \
+                        --eta "${eta_list[@]}" \
+                        --seed $seed \
+                        # --orthogonal \
+                        # --processes \
+                        # --vv
+                done
+            done
+        # done
+    done
+done
 
 for isnr in 35 25 45
 do
@@ -110,23 +141,3 @@ do
         done
     done
 done
-
-# encoder="standard"
-# algorithm="TSOC"
-
-# for isnr in 35 25 45
-# do
-#     echo $isnr $encoder $algorithm
-#     python experiments/compute_rsnr.py \
-#         --measurements "${m_list[@]}" \
-#         --seed "${seed_list[@]}" \
-#         --isnr $isnr \
-#         --algorithm $algorithm \
-#         --encoder $encoder \
-#         --orthogonal \
-#         --correlation $corr \
-#         --localization $loc \
-#         --eta "${eta_list[@]}" \
-#         # -vv \
-#         # --processes \
-# done
